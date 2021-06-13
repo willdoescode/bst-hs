61bddb545bf8e2ad80eaebf4d38dc941bd4d2e2d
Will Lane <williamlane923@gmail.com>
2021-06-12 23:21:40 -0700

    added tests

--- Main.hs
+++ Main.hs
@@ -1,11 +1,13 @@
 module Main where
 
+import qualified Test.HUnit as T
+
 data Node a = Node
   { value :: a,
     right :: Maybe (Node a),
     left :: Maybe (Node a)
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 newList :: a -> Node a
 newList x = Node {value = x, right = Nothing, left = Nothing}
@@ -43,8 +45,43 @@ unwrap Nothing = error "handle error"
 invert :: Node a -> Node a
 invert = unwrap . invert' . pure
 
--- main :: IO ()
--- main = print $ insert (insert (insert (newList 5) 6) 7) 3
+testCaseAnswer, invertedTestCaseAnswer :: Num a => Node a
+testCaseAnswer = Node {value = 5, right = Just (Node {value = 6, right = Just (Node {value = 7, right = Nothing, left = Nothing}), left = Nothing}), left = Just (Node {value = 3, right = Nothing, left = Nothing})}
+invertedTestCaseAnswer = Node {value = 5, right = Just (Node {value = 3, right = Nothing, left = Nothing}), left = Just (Node {value = 6, right = Nothing, left = Just (Node {value = 7, right = Nothing, left = Nothing})})}
 
-main :: IO ()
-main = print $ invert $ insertList (newList 5) [6, 7, 3] -- == insert (insert (insert (newList 5) 6) 7) 3
+testInsert :: T.Test
+testInsert =
+  T.TestCase $
+    T.assertEqual
+      "BST Should insert correctly"
+      (insert (insert (insert (newList 5) 6) 7) 3)
+      testCaseAnswer
+
+testInsertList :: T.Test
+testInsertList =
+  T.TestCase $
+    T.assertEqual
+      "BST Should insert list correctly"
+      ( insertList
+          (newList 5)
+          [6, 7, 3]
+      )
+      testCaseAnswer
+
+testInvertlist :: T.Test
+testInvertlist =
+  T.TestCase $
+    T.assertEqual
+      "BST Should invert"
+      ( invert $
+          insertList
+            (newList 5)
+            [6, 7, 3]
+      )
+      invertedTestCaseAnswer
+
+main :: IO T.Counts
+main = T.runTestTT $ T.TestList [testInsert, testInsertList, testInvertlist]
+
+-- main :: IO ()
+-- main = print $ invert $ insertList (newList 5) [6, 7, 3] -- == insert (insert (insert (newList 5) 6) 7) 3
