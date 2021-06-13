cff074ca2b74abcf8cfafa4fec7eef2d6758f534
Will Lane <williamlane923@gmail.com>
2021-06-13 01:42:17 -0700

    formatting

--- Bst.hs
+++ Bst.hs
@@ -19,7 +19,12 @@ data Node a = Node
   deriving (Show, Eq)
 
 newList :: a -> Node a
-newList x = Node {value = x, right = Nothing, left = Nothing}
+newList x =
+  Node
+    { value = x,
+      right = Nothing,
+      left = Nothing
+    }
 
 insert' :: Ord a => Maybe (Node a) -> a -> Node a
 insert' Nothing x = newList x
@@ -45,14 +50,20 @@ insertList = foldl insert
 
 invert' :: Maybe (Node a) -> Maybe (Node a)
 invert' Nothing = Nothing
-invert' (Just head) = Just $ Node {value = value head, right = invert' (left head), left = invert' (right head)}
+invert' (Just head) =
+  Just $
+    Node
+      { value = value head,
+        right = invert' (left head),
+        left = invert' (right head)
+      }
 
 invert :: Node a -> Node a
 invert = fromJust . invert' . pure
 
 height' :: Maybe (Node a) -> Int
 height' Nothing = 0
-height' (Just Node {value = _, right = r, left = l}) = 1 + max (height' r) (height' l)
+height' (Just Node {right = r, left = l}) = 1 + max (height' r) (height' l)
 
 height :: Node a -> Int
 height = height' . pure
@@ -61,7 +72,13 @@ remove' :: Eq a => Maybe (Node a) -> a -> Maybe (Node a)
 remove' Nothing _ = Nothing
 remove' (Just Node {value = val, right = r, left = l}) v
   | val == v = Nothing
-  | otherwise = Just $ Node {value = val, right = remove' r v, left = remove' l v}
+  | otherwise =
+    Just $
+      Node
+        { value = val,
+          right = remove' r v,
+          left = remove' l v
+        }
 
 remove :: Eq a => Node a -> a -> Node a
 remove head v = fromJust $ remove' (pure head) v
