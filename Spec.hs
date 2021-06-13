8043c80b2bc6750673a99513b93f614230094a28
Will Lane <williamlane923@gmail.com>
2021-06-13 01:38:16 -0700

    hiding (Node)

--- Spec.hs
+++ Spec.hs
@@ -1,23 +1,23 @@
 import Bst
-import qualified Test.HUnit as T
+import Test.HUnit hiding (Node)
 
 testCaseAnswer, testInvertedAnswer, testRemoveAnswer :: Num a => Node a
 testCaseAnswer = Node {value = 5, right = Just (Node {value = 6, right = Just (Node {value = 7, right = Nothing, left = Nothing}), left = Nothing}), left = Just (Node {value = 3, right = Nothing, left = Nothing})}
 testInvertedAnswer = Node {value = 5, right = Just (Node {value = 3, right = Nothing, left = Nothing}), left = Just (Node {value = 6, right = Nothing, left = Just (Node {value = 7, right = Nothing, left = Nothing})})}
 testRemoveAnswer = Node {value = 5, right = Just (Node {value = 6, right = Nothing, left = Nothing}), left = Just (Node {value = 3, right = Nothing, left = Nothing})}
 
-testInsert :: T.Test
+testInsert :: Test
 testInsert =
-  T.TestCase $
-    T.assertEqual
+  TestCase $
+    assertEqual
       "BST Should insert correctly"
       testCaseAnswer
       (insert (insert (insert (newList 5) 6) 7) 3)
 
-testInsertList :: T.Test
+testInsertList :: Test
 testInsertList =
-  T.TestCase $
-    T.assertEqual
+  TestCase $
+    assertEqual
       "BST Should insert list correctly"
       testCaseAnswer
       ( insertList
@@ -25,10 +25,10 @@ testInsertList =
           [6, 7, 3]
       )
 
-testInvertTree :: T.Test
+testInvertTree :: Test
 testInvertTree =
-  T.TestCase $
-    T.assertEqual
+  TestCase $
+    assertEqual
       "BST Should invert"
       testInvertedAnswer
       ( invert $
@@ -37,24 +37,24 @@ testInvertTree =
             [6, 7, 3]
       )
 
-testHeight :: T.Test
+testHeight :: Test
 testHeight =
-  T.TestCase $
-    T.assertEqual
+  TestCase $
+    assertEqual
       "Height should be correct"
       3
       (height $ insertList (newList 5) [6, 7, 3])
 
-testRemove :: T.Test
+testRemove :: Test
 testRemove =
-  T.TestCase $
-    T.assertEqual
+  TestCase $
+    assertEqual
       "Removal from list should work"
       testRemoveAnswer
       (remove (insertList (newList 5) [6, 7, 8, 9, 3]) 7)
 
-tests :: T.Test
-tests = T.TestList [testInsert, testInsertList, testInvertTree, testHeight, testRemove]
+tests :: Test
+tests = TestList [testInsert, testInsertList, testInvertTree, testHeight, testRemove]
 
-main :: IO T.Counts
-main = T.runTestTT tests
+main :: IO Counts
+main = runTestTT tests
